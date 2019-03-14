d2a514a91d4d2ca4b45add73dcb92db994b26e6a
Alan McElroy <34488920+alanislegend@users.noreply.github.com>
2019-03-14 02:03:43 -0400

    push

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    //Reference to the rigidbody component on player character (rb)
+    public Rigidbody rb;
+
+    //Public variable you can change in unity attached to player character for speeds
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+	
+	// using FixedUpdate for better performance for physics based code. 
+    // Using Time.deltaTime for balanced performance on all systems regardless of FPS for player.
+    // Code for forward movement/speed of player character.
+	void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
+
+    }
+}
