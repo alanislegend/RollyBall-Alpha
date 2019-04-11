84087cda27d5c2c0c970fb1460f91cd4a8954b5c
Alan McElroy <alan.mcelroy1@yahoo.com>
2019-04-11 04:30:27 -0400

    New Stuff
    Added level 1 design (more objects) and better physics/character controller

---
+++ I Am The Fast/Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    //Gathers Player position and offsets camera to follow behind a certain amount
+    public Transform player;
+    public Vector3 offset;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + offset;
+		
+	}
+}
